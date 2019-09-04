b351621efa044208e2aee2ecbb0e650e7f724bed
Alexsey <agat00@gmail.com>
2019-09-04 23:59:56 +0300

    tests - change most tests according to a new idea

--- test/index.test.js
+++ test/index.test.js
@@ -7,127 +7,82 @@ describe('Plenum function ->', () => {
     expect(plenum).toBeFunction()
   })
 
-  describe('First argument is an object ->', () => {
-    it('Should create a new plenum when all properties are strings', () => {
-      const days = plenum({
-        mon: 'Monday',
-        tue: 'Tuesday',
-        wed: 'Wednesday',
-        thu: 'Thursday',
-        fri: 'Friday',
-        sat: 'Saturday',
-        sun: 'Sunday',
-      })
-
-      expect(days).toHaveProperty('mon', days.mon)
-      expect(days).toHaveProperty('tue', days.tue)
-      expect(days).toHaveProperty('wed', days.wed)
-      expect(days).toHaveProperty('thu', days.thu)
-      expect(days).toHaveProperty('fri', days.fri)
-      expect(days).toHaveProperty('sat', days.sat)
-      expect(days).toHaveProperty('sun', days.sun)
+  describe('First argument is an array ->', () => {
+    it('Should throw if first argument is not an array', () => {
+      expect(() => plenum('bla'))
+        .toThrow('First argument to plenum must be an array but provided "bla"')
     })
+    it('Should not throw if first argument is an array of strings', () => {
+      expect(() => plenum(['bla'])).not.toThrow()
+    })
+    it('Should not throw if first argument is an array of numbers', () => {
+      expect(() => plenum([1, 2, 3])).not.toThrow()
+    })
+    it('Should not throw if first argument is an array of String objects', () => {
+      expect(() => plenum([new String('a'), new String('b')])).not.toThrow()
+    })
+    it('Should not throw if first argument is an array of mix of allowed elements', () => {
+      expect(() => plenum([new String('a'), 'b', 3])).not.toThrow()
+    })
+    it('Should convert number values to strings', () => {
+      const primesTill10Numbers = plenum([1, 2, 3, 4, 5])
+      const primesTill10Strings = plenum(['1', '2', '3', '4', '5'])
 
-    it.only('Should convert number values to strings', () => {
-      const primesTill10Numbers = plenum({
-        'two': 2,
-        'three': 3,
-        'five': 5,
-        'seven': 7,
-      })
-      const primesTill10Strings = plenum({
-        'two': '2',
-        'three': '3',
-        'five': '5',
-        'seven': '7',
-      })
-
-      expect(primesTill10Numbers).toEqual(primesTill10Strings)
+      expect(primesTill10Numbers).toEqualPlenum(primesTill10Strings)
     })
   })
-
-   describe('First argument is an array ->', () => {
-     it('Should be the same as creation from object ' +
-       'when all elements or array are strings and keys of object and equal to its values', (
-     ) => {
-       const daysFromArray = plenum(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'])
-       const daysFromObj = plenum({
-         mon: 'mon',
-         tue: 'tue',
-         wed: 'wed',
-         thu: 'thu',
-         fri: 'fri',
-         sat: 'sat',
-         sun: 'sun',
-       })
-
-       expect(daysFromArray).toEqual(daysFromObj)
-     })
-
-     it('Should convert numbers elements to strings', () => {
-       const primesTill10Numbers = plenum([2, 3, 5, 7])
-       const primesTill10Strings = plenum(['2', '3', '5', '7'])
-
-       expect(primesTill10Numbers).toEqual(primesTill10Strings)
-     })
-   })
 })
 
 describe('Plenum set ->', () => {
   describe('prototype', () => {
     it('Should be set by second argument of plenum function', () => {
-      const prototype = {}
+      const prototype = {specialField: () => {}}
 
-      const days = plenum({
-        mon: 'Monday',
-        tue: 'Tuesday',
-        wed: 'Wednesday',
-        thu: 'Thursday',
-        fri: 'Friday',
-        sat: 'Saturday',
-        sun: 'Sunday',
-      }, prototype)
+      const times = plenum(['winter', 'spring', 'summer', 'autumn'], prototype)
 
-      expect(Object.getPrototypeOf(days)).toBe(prototype)
+      expect(Object.getPrototypeOf(times)).toEqual(prototype)
     })
 
     it('Should be set by second argument of plenum function', () => {
-      const days = plenum({
-        mon: 'Monday',
-        tue: 'Tuesday',
-        wed: 'Wednesday',
-        thu: 'Thursday',
-        fri: 'Friday',
-        sat: 'Saturday',
-        sun: 'Sunday',
-      }, {
-        get isWeekend () {
-          return this.is.sat.or.sun
+      const times = plenum(['winter', 'spring', 'summer', 'autumn'], {
+        get isHot () {
+          return this.is.spring.or.summer
         }
       })
 
-      expect(days.mon.isWeekend).toBeFalsy()
-      expect(days.sat.isWeekend).toBeTruthy()
+      expect(times.winter.isHot).toBeFalsy()
+      expect(times.summer.isHot).toBeTruthy()
     })
+  })
+
+  it('Should be instanceof Array', () => {
+    const times = plenum(['winter', 'spring', 'summer', 'autumn'])
+
+    expect(times).toBeInstanceOf(Array)
+  })
+
+  // todo think over
+  it.skip('Should be instanceof plenum function', () => {
+    const times = plenum(['winter', 'spring', 'summer', 'autumn'])
+
+    expect(times).toBeInstanceOf(plenum)
   })
 })
 
 describe('Plenum instance ->', () => {
-  it('Should be instanceof plenum function', () => {
-    const days = plenum({
-      mon: 'Monday',
-      tue: 'Tuesday',
-      wed: 'Wednesday',
-      thu: 'Thursday',
-      fri: 'Friday',
-      sat: 'Saturday',
-      sun: 'Sunday',
-    })
+  it('Should be instanceof String', () => {
+    const times = plenum(['winter', 'spring', 'summer', 'autumn'])
 
-    expect(days.mon).toBeInstanceOf(plenum)
+    expect(times.winter).toBeInstanceOf(String)
+  })
+  // todo think over below
+  it.skip('Should be instanceof plenum function', () => {
+    const times = plenum(['winter', 'spring', 'summer', 'autumn'])
+
+    expect(times.winter).toBeInstanceOf(plenum)
   })
 
-  it('Should be instanceof plenum set it is a part of', () => {
+  it.skip('Should be instanceof plenum set it is a part of', () => {
     const days = plenum({
       mon: 'Monday',
       tue: 'Tuesday',
@@ -141,7 +96,7 @@ describe('Plenum instance ->', () => {
     expect(days.mon).toBeInstanceOf(days)
   })
 
-  it('Should not be instanceof plenum set it is not a part of', () => {
+  it.skip('Should not be instanceof plenum set it is not a part of', () => {
     const days = plenum({
       mon: 'Monday',
       tue: 'Tuesday',
